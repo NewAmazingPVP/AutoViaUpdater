2b2b955db0229ea4cab5bbf7684c5b34160fa2e9
NewAmazingPVP <gamingofpat@gmail.com>
2023-12-31 16:21:49 -0500

    /update command for update checker
    /update command for update checker

--- src/main/java/spigot/AutoViaUpdater.java
+++ src/main/java/spigot/AutoViaUpdater.java
@@ -1,6 +1,10 @@
 package spigot;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -34,6 +38,7 @@ public final class AutoViaUpdater extends JavaPlugin {
         isViaRewindDev = getConfig().getBoolean("ViaRewind.dev");
         isViaRewindLegacyEnabled = getConfig().getBoolean("ViaRewind-Legacy.enabled");
         updateChecker();
+        getCommand("updatevias").setExecutor(new UpdateCommand());
     }
 
     public void updateChecker() {
@@ -42,30 +47,34 @@ public final class AutoViaUpdater extends JavaPlugin {
         Bukkit.getScheduler().runTaskTimerAsynchronously(this, new Runnable() {
             @Override
             public void run() {
-                try {
-                    if (isViaVersionEnabled && !isViaVersionDev) {
-                        updateVia("ViaVersion", getDataFolder().getParent(), false);
-                    } else if (isViaVersionEnabled && isViaVersionDev) {
-                        updateVia("ViaVersion-Dev", getDataFolder().getParent(), true);
-                    }
-                    if (isViaBackwardsEnabled && !isViaBackwardsDev) {
-                        updateVia("ViaBackwards", getDataFolder().getParent(), false);
-                    } else if (isViaBackwardsEnabled && isViaBackwardsDev) {
-                        updateVia("ViaBackwards-Dev", getDataFolder().getParent(), true);
-                    }
-                    if (isViaRewindEnabled && !isViaRewindDev) {
-                        updateVia("ViaRewind", getDataFolder().getParent(), false);
-                    } else if (isViaRewindEnabled && isViaRewindDev) {
-                        updateVia("ViaRewind-Dev", getDataFolder().getParent(), true);
-                    }
-                    if (isViaRewindLegacyEnabled) {
-                        updateVia("ViaRewind%20Legacy%20Support", getDataFolder().getParent(), false);
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+                checkUpdateVias();
             }
         }, 0L, 20L * 60L * interval);
+    }
+
+    public void checkUpdateVias(){
+        try {
+            if (isViaVersionEnabled && !isViaVersionDev) {
+                updateVia("ViaVersion", getDataFolder().getParent(), false);
+            } else if (isViaVersionEnabled && isViaVersionDev) {
+                updateVia("ViaVersion-Dev", getDataFolder().getParent(), true);
+            }
+            if (isViaBackwardsEnabled && !isViaBackwardsDev) {
+                updateVia("ViaBackwards", getDataFolder().getParent(), false);
+            } else if (isViaBackwardsEnabled && isViaBackwardsDev) {
+                updateVia("ViaBackwards-Dev", getDataFolder().getParent(), true);
+            }
+            if (isViaRewindEnabled && !isViaRewindDev) {
+                updateVia("ViaRewind", getDataFolder().getParent(), false);
+            } else if (isViaRewindEnabled && isViaRewindDev) {
+                updateVia("ViaRewind-Dev", getDataFolder().getParent(), true);
+            }
+            if (isViaRewindLegacyEnabled) {
+                updateVia("ViaRewind%20Legacy%20Support", getDataFolder().getParent(), false);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void loadConfiguration(){
@@ -83,5 +92,15 @@ public final class AutoViaUpdater extends JavaPlugin {
 
         config.options().copyDefaults(true);
         saveConfig();
+    }
+
+    public class UpdateCommand implements CommandExecutor {
+
+        @Override
+        public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+            checkUpdateVias();
+            sender.sendMessage(ChatColor.AQUA + "Update checker for vias successful!");
+            return true;
+        }
     }
 }
