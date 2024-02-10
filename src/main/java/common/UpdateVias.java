52deb23dca729fa6080c363a70d6905b844e1f78
NewAmazingPVP <87737759+NewAmazingPVP@users.noreply.github.com>
2024-02-09 22:16:23 -0500

    Update UpdateVias.java

--- src/main/java/common/UpdateVias.java
+++ src/main/java/common/UpdateVias.java
@@ -59,7 +59,7 @@ public class UpdateVias {
             if (files != null) {
                 boolean containsVia = false;
                 for (File file : files) {
-                    if (file.getName().toLowerCase().contains(s.toLowerCase().replace("-dev", "").replace("%20", "-"))) {
+                    if (!file.isDirectory() && file.getName().toLowerCase().contains(s.toLowerCase().replace("-dev", "").replace("%20", "-"))) {
                         containsVia = true;
                         break;
                     }
@@ -68,7 +68,7 @@ public class UpdateVias {
                         break;
                     }*/
                 }
-                if (!containsVia) {
+                if (containsVia) {
                     outputFilePath = directory + "/update/" + s + ".jar";
                 }
             }
